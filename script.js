e5fa80065db9f5b44f70aef23cf9e742191a5776
test <test>
2022-11-15 15:30:59 +0300

    Java

---
+++ script.js
@@ -0,0 +1,12 @@
+var modal =prompt("Ismingizni kiriting","name")
+var ism = "Otabek"
+console.log("Mening ismim "+ism);
+var age =prompt("Yoshingizni kiriting")
+var age = 17
+console.log("Men "+age+" yoshdaman");
+console.log("Misol 1 : "+"6 + 3 = "+(6+3)+" Sizning javob "+prompt("Misolni yeching"+" 6 + 3 = ?"));
+console.log("Misol 2 : "+"20 - 5 = "+(20-5)+" Sizning javob "+prompt("Misolni yeching"+" 20 - 5 = ?"));
+console.log("Misol 3 : "+"15 * 2 = "+(15*2)+" Sizning javob "+prompt("Misolni yeching"+" 15 * 2 = ? "));
+console.log("Misol 4 : "+"8 / 4 = "+(8/4)+" Sizning javob "+prompt("Misolni yeching"+" 8 / 4 = ?"));
+console.log("Misol 5 : "+"10 % 3 = "+(10%3)+" Sizning javob "+prompt("Misolni yeching"+" 10 % 3 = ?"));
+
